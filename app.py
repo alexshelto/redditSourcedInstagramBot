df09f238f738e75ce068f8daf0685f39406e5f96
alexshelto <alexandershelton@icloud.com>
2019-05-31 01:13:20 -0500

    Functioning Bot v0

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-#Alex Shelton
-
-
+#Alexnader Shelton
+#Meme grabber program
+# pulls memes from reddit to be posted to an instagram account every hr and a half
 
 from InstagramAPI import InstagramAPI
 import praw
@@ -14,16 +14,17 @@ import time
 import hashlib
 import sys #arguments to the script
 
-#reddit preferences / app to create a script api, this is where
-#these credentials are from
 
-client_id= ''
-client_secret = ''
-password=''
-user_agent = ''
-username = ''
+           #visit reddit preferences api
+######### Reddit API Credentials #############################
+client_id= 'bggfuF1vKehqyg'
+client_secret='pxIaMYmHVN3i28oYKk-D1qGv9_g'
+password='memeanddestroy'
+user_agent = 'memePuller'
+username = 'almightyMeme'
+##############################################################
 
-path = ' ' #path to directory
+path = '' #Path of where files should be saved to
 
 #Instagram username & Password is passed into program through arguments given to code
 insta_user = sys.argv[1]
@@ -32,8 +33,9 @@ insta_pswd = sys.argv[2]
 
 subreddit_list = ['memes','dank_meme']
 
-captionList = ['Beans','Premium memes posted all day ever day', 'If I punch myself and it hurts does that make me strong or weak?']
+captionList = ['Beans','Premium memes posted all day every day','I post memes all day', 'All I do is make and browse memes', 'My memes are superior just admit it','Share this with 5 homies', 'I need to get off the internet', 'Krumpit','Fart','ayy lmao', 'Haha rawr XD', 'If I punch myself and it hurts does that make me strong or weak?']
 
+tags = '#dankmemes #memes #meme #dank #funny #lol #cringe #edgy #dankmeme #edgymemes #funnymemes #anime #memesdaily #lmfao #follow #comedy #hilarious #nochill #ayylmao #triggered #fortnite #savage'
 
 files = []
 
@@ -42,14 +44,34 @@ reddit = praw.Reddit(client_id=client_id,
                     user_agent=user_agent, username=username)
 
 
+
+
+
+
+
+
+
+def addTags():
+    instaBot.login() #log in
+    user_posts = instaBot.getSelfUserFeed()
+    feed = instaBot.LastJson #load json data
+    latestMediaItem = feed["items"][0] #pull index 0 - most recent post
+    latestMediaItemID = latestMediaItem["caption"]["media_id"] #capture media id
+
+    instaBot.comment(mediaId=latestMediaItemID, commentText=tags)
+    print("Tags added to photo")
+    instaBot.logout()
+
+
 def postImage():
     randomImage = random.randrange(0,len(files))
-    randomCaption = random.randrange(0,len(captionLen))
+    randomCaption = random.randrange(0,len(captionList))
 
     desiredFile = files[randomImage]
     desiredCaption = captionList[randomCaption]
     instaBot.login()
     instaBot.uploadPhoto(desiredFile, caption=desiredCaption)
+
     instaBot.logout()
     print("Image has been posted")
     #delete file from directory and list:
@@ -90,8 +112,6 @@ def encodeName(post):
 
 
 def save(post):
-    title = post.title
-
     url = post.url
     image = requests.get(url).content
     imageName = encodeName(post)
@@ -108,7 +128,7 @@ def save(post):
 def wipeAllFiles():
     for file in files:
         os.remove(file)
-    files = []
+    files.clear()
 
 
 
@@ -124,6 +144,10 @@ def retreivePhotos():
 
 
 
+
+
+
+
 if __name__ == '__main__':
     #Creating bot
     instaBot = InstagramAPI(insta_user,insta_pswd)
@@ -136,6 +160,7 @@ if __name__ == '__main__':
 
         time.sleep(6000) # sleeping for 1.5 hours (the break betweeen the posts)
         postImage()
+        addTags()
         postCount += 1
 
         if postCount % 7 == 0: #every 7 posts do this:
